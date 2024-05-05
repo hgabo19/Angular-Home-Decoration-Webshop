235f3ebd5e2d57ccdc4719777de360ec62a68c35
Gabooka19 <huszka.gabor2002@gmail.com>
2024-05-05 18:27:24 +0200

    fixing login error because of missing validators.

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Form, FormControl } from '@angular/forms';
+import { Form, FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/shared/services/auth.service';
 
@@ -9,22 +9,26 @@ import { AuthService } from 'src/app/shared/services/auth.service';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
-  email = new FormControl('');
-  password = new FormControl('');
+  email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('', Validators.required);
   constructor(private authService: AuthService, private router: Router) {}
 
   login() {
-    this.authService
-      .login(this.email?.value as string, this.password?.value as string)
-      .then(() => {
-        alert('Welcome to the page!');
-        this.email.setValue('');
-        this.password.setValue('');
-        this.router.navigateByUrl('decorations');
-      })
-      .catch(() => {
-        alert('Wrong email or password!');
-        return;
-      });
+    if (this.email.valid && this.password.valid) {
+      this.authService
+        .login(this.email?.value as string, this.password?.value as string)
+        .then(() => {
+          alert('Welcome to the page!');
+          this.email.setValue('');
+          this.password.setValue('');
+          this.router.navigateByUrl('decorations');
+        })
+        .catch(() => {
+          alert('Wrong email or password!');
+          return;
+        });
+    } else {
+      alert('Wrong email or password!');
+    }
   }
 }
